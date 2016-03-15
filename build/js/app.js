1fcacc12fbc808579f044c9fd5c8778655cc8c1e
Michael Schennum and Tyler Suderman <student@epicodus.com>
2016-03-15 09:32:06 -0700

    adds full function weather api

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "6dbfb1db59dc2b72dd48a4829420f794";
+
+},{}],2:[function(require,module,exports){
 exports.pingPong = function(goal) {
   var output = [];
   for (var i = 1; i <= goal; i++) {
@@ -15,7 +18,7 @@ exports.pingPong = function(goal) {
   return output;
 };
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 var pingPong = require('./../js/ping-pong.js').pingPong;
 
 $(document).ready(function(){
@@ -42,4 +45,18 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-},{"./../js/ping-pong.js":1}]},{},[2]);
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.message);
+    });
+  });
+});
+
+},{"./../.env":1,"./../js/ping-pong.js":2}]},{},[3]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -23,3 +23,17 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#time').text(moment());
 });
+
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.message);
+    });
+  });
+});
