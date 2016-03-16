3f20983468ba055eb3a90d448a8371f7a5a0146e
Michael Schennum and Polina Nenchev <polina.mihova@yahoo.com>
2016-03-16 14:16:45 -0700

    adds two buttons

--- js/google.js
+++ js/google.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 exports.Google = function() {
 };
 
@@ -60,18 +59,3 @@ exports.Google.prototype.bicycle = function() {
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
 }
-
-},{}],2:[function(require,module,exports){
-var Google = require('./../js/google.js').Google;
-
-$(document).ready(function() {
-  var newGoogle = new Google();
-  $('#locateUser').click(function() {
-    newGoogle.initMap();
-  });
-  $('#locateLocal').click(function() {
-    newGoogle.bicycle();
-  });
-});
-
-},{"./../js/google.js":1}]},{},[2]);
--- js/location-interface.js
+++ js/location-interface.js
@@ -1,50 +1,11 @@
+var Google = require('./../js/google.js').Google;
+
 $(document).ready(function() {
-  $('#locateUser').click(locateUser);
+  var newGoogle = new Google();
+  $('#locateUser').click(function() {
+    newGoogle.initMap();
+  });
+  $('#locateLocal').click(function() {
+    newGoogle.bicycle();
+  });
 });
-
-function locateUser() {
-  if (navigator.geolocation) {
-    var positionOptions = {
-      enableHighAccuracy: true,
-      timeout: 10 * 1000 // 10 seconds
-    };
-
-    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
-    }
-    else {
-    alert("Your browser doesn't support the Geolocation API");
-    }
-  }
-
-
-  // this is the success callback from telling the navigator (your browser) to get the current user's position
-  // we do this on line 13 above. We pass in a function to call on success, a function to call on error, and some options to tell the geolocation api how we want it to run.
-  // on successfully locating the user, geolocationSuccess() gets called automatically, and it is passed the user's position as an argument.
-  // on error, geolocationError is called.
-
-  function geolocationSuccess(position) {
-    // here we take the `position` object returned by the geolocation api
-    // and turn it into google maps LatLng object by calling the google.maps.LatLng constructor function
-    // it 2 arguments: one for latitude, one for longitude.
-    // You could refactor this section to pass google maps your own coordinates rather than using geolocation for the user's current location.
-    // But you must use coordinates to use this method.
-
-    var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-    var myOptions = {
-      zoom : 16,
-      center : userLatLng,
-      mapTypeId : google.maps.MapTypeId.ROADMAP
-    };
-    // Draw the map - you have to use 'getElementById' here.
-    var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-    // Place the marker
-    new google.maps.Marker({
-      map: mapObject,
-      position: userLatLng
-    });
-  }
-
-  function geolocationError(positionError) {
-    alert(positionError);
-  }
